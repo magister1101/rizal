aa1c0ab7cbd3c495722ca17f300e74d547edbff0
MarkVanMortem <34596876+magister1101@users.noreply.github.com>
2024-06-16 14:05:56 +0800

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rizal</title>
+    <link rel="stylesheet" href="css/bootstrap-5.3.3-dist/css/bootstrap-grid.min.css">
+</head>
+
+<body>
+    <button type="button" class="btn btn-primary">Primary</button>
+</body>
+
+</html>
